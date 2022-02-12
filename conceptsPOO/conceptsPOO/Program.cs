b7b52b3fb2b706cae4ee726e323150ff2ba60755
jose hernando perea <johevape03@hotmail.com>
2022-02-12 15:28:24 -0500

    finish date class

---
+++ conceptsPOO/conceptsPOO/Program.cs
@@ -0,0 +1,13 @@
+﻿using conceptsPOO;
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(2022, 12, 12));
+    Console.WriteLine(new Date(2022, 2, 21));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
